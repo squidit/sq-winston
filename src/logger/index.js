923c42688ec8cbe01d4702736e8a647b0f6fbf26
Vinicius Spagnol <vinicius.spagnol@gmail.com>
2020-06-01 20:40:44 -0300

    :sparkles: Add new index to write when impersonate request

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,24 +1,32 @@
 const { createLogger } = require('winston')
+const { get } = require('lodash')
 const { mapStep } = require('../utils/map-step')
 const { mapRequest } = require('sq-winston/src/utils/map-request-hapi')
 const shortid = require('shortid')
-const elasticTransport = require('./transports/elastic.transport')
+const {
+  elasticTransport,
+  elasticTransportImpersonate
+ } = require('./transports/elastic.transport')
 
 const key = 'sq-traceId'
 
 /**
  * Retorna todos os transporter que serão utilizados pelo Winston
  */
-function getTransports () {
-  const transports = [elasticTransport]
+function getTransports (type) {
+  const transports = type === 'impersonate' ? [elasticTransportImpersonate] : [elasticTransport]
   return transports.filter(t => t !== null)
 }
 
 /**
- * Cria o objeto de log do Winston
+ * Cria o objetos de log do Winston
  */
 const logger = createLogger({
   transports: getTransports()
+})
+
+const loggerImpersonate = createLogger({
+  transports: getTransports('impersonate')
 })
 
 /**
@@ -27,12 +35,15 @@ const logger = createLogger({
 class Logger {
   static error (message, meta = {}) {
     logger.log('error', message, meta)
+    if (!!get(meta, 'authentication.impersonate')) loggerImpersonate.log('error', message, meta)
   }
   static warn (message, meta = {}) {
     logger.log('warn', message, meta)
+    if (!!get(meta, 'authentication.impersonate')) loggerImpersonate.log('warn', message, meta)
   }
   static info (message, meta = {}) {
     logger.log('info', message, meta)
+    if (!!get(meta, 'authentication.impersonate')) loggerImpersonate.log('info', message, meta)
   }
   static step (message, stepInfo = {}, options = {}) {
     const request = options.request || {}
@@ -43,6 +54,7 @@ class Logger {
     const meta = { ...metaStep, ...metaRequest, options }
     if (meta.traceId) meta.traceId = shortid.generate()
     logger.log(metaStep.type, message, meta)
+    if (!!get(meta, 'authentication.impersonate')) loggerImpersonate.log(metaStep.type, message, meta)
     return meta.traceId
   }
 }
