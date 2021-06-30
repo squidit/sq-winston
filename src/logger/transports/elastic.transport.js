55f3b2dc38c01916ecab643412fb39e58f58a692
Pixote <guiihpr@gmail.com>
2021-06-30 16:22:28 -0300

    Redirecionando logs de pagamentos para um unico indice Redirecionando logs de impersonate para um unico indice

--- src/logger/transports/elastic.transport.js
+++ src/logger/transports/elastic.transport.js
@@ -48,19 +48,25 @@ function getESOptions () {
   return options
 }
 
-const elasticTransport = ELASTIC_LOG_URL
-  ? new WinstonES({
+let elasticTransport = null
+if (ELASTIC_LOG_URL) {
+  const opts = {
     client: new elasticsearch.Client(getESOptions()),
-    transformer,
-    indexPrefix: ELASTIC_LOG_INDEX_PREFIX ? ELASTIC_LOG_INDEX_PREFIX : 'logs'
-  })
-  : null
+    transformer
+  }
+  if (ELASTIC_LOG_INDEX_PREFIX && ELASTIC_LOG_INDEX_PREFIX.includes('payments')) {
+    opts.index = ELASTIC_LOG_INDEX_PREFIX
+  }
+
+  elasticTransport = new WinstonES(opts)
+}
+
 
 const elasticTransportImpersonate = ELASTIC_LOG_URL
   ? new WinstonES({
     client: new elasticsearch.Client(getESOptions()),
     transformer,
-    indexPrefix: 'impersonate'
+    index: 'impersonate'
   })
   : null
 
